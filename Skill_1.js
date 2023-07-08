a9ac4fd149635f09103eac0ab510184805743f1b
k-yogabala <134762935+k-yogabala@users.noreply.github.com>
2023-07-08 20:55:06 +0530

    Delete Skill_1.js

--- Skill_1.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', (request, response) => {
-    response.send('<h1>Hello Cohort2 Part3 SectionC!</h1>')
-})
-app.listen(8081)
-console.log(`Srever started ${8081}`)
